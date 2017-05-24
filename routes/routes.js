5f9b9413337feccaed336df4a10aecfef0027ac8
Thobias N. Hejstvig-Larsen <thobiasn@users.noreply.github.com>
2017-05-24 15:31:33 +0200

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -7,13 +7,13 @@ var express = require('express'),
 var indexController = require(path+'indexController');
 
 // Models
-var entry = require('../models/entry');
+var entryModel = require('../models/entry');
 
 // Routes
 router.route('/').get(indexController.index);
 
-entry.methods(['get', 'put', 'post', 'delete']);
-entry.register(router, '/entries');
+entryModel.methods(['get', 'put', 'post', 'delete']);
+entryModel.register(router, '/entries');
 
 // Export the router object
 // Apparently you can simply export the object itself and the changes will occur. Is the object static and so, being changed in this file, which then returns the modified object and changes it in the app js file?
